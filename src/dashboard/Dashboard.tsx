175971cf823dc03526a63aa28c0f7415ed2f778d
Ben Shabowski <benshabowski@gmail.com>
2026-06-12 11:37:48 -0500

    feat: enhance Dashboard layout with responsive design and add click effect to ServerCard

--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -1,12 +1,15 @@
 import {useServerData} from "../global/server/useServerData.ts";
 import ServerCard from "./ServerCard.tsx";
+import {Box, Stack} from "@mui/material";
 
 function Dashboard() {
   const { serverData } = useServerData();
 
-  return <>
-    {serverData.map(server => <ServerCard data={server} key={server.id}/>)}
-  </>;
+  return <Box sx={{padding: 2}}>
+    <Stack direction={'row'} spacing={1}>
+      {serverData.map(server => <ServerCard data={server} key={server.id}/>)}
+    </Stack>
+  </Box>;
 }
 
 export default Dashboard;
