f64b66c14563653967a7c024ca3baad6934aecf2
zabory <benshabowski@gmail.com>
2026-06-12 08:00:18 -0500

    feat: enhance ServerCard with player count display and status icon

--- src/dashboard/ServerCard.tsx
+++ src/dashboard/ServerCard.tsx
@@ -1,6 +1,7 @@
 import type {ServerData} from "../global/server/ServerDataProvider.tsx";
-import {Card, CardContent, Stack, Typography} from "@mui/material";
+import {Card, CardContent, Divider, Stack, Typography} from "@mui/material";
 import MinecraftHead from "../components/MinecraftHead.tsx";
+import { RiProgress8Line } from "react-icons/ri";
 
 type ServerCardProps = {
   data: ServerData;
@@ -9,16 +10,26 @@ type ServerCardProps = {
 function ServerCard({data}: ServerCardProps){
   return <Card sx={{ maxWidth: 345 }}>
     <CardContent>
-      <Typography variant={'h4'}>{data.name}</Typography>
-      <Typography>{data.pingData ? 'Up' : 'Down'}</Typography>
-      {data.pingData && data.pingData.players.sample ? <>
-      {data.pingData.players.sample.map(player => {
-        return <Stack direction={'row'} spacing={1} key={player.id} sx={{alignItems: 'center'}}>
-          <MinecraftHead uuid={player.id}/>
-          <Typography>{player.name}</Typography>
-        </Stack>;
-      })}
-      </> : undefined}
+      <Stack direction={'row'} sx={{alignItems: 'center'}} spacing={1} >
+        <Typography variant={'h4'}>{data.name}</Typography>
+        <RiProgress8Line size={25} color={data.pingData ? 'green' : 'red'} />
+      </Stack>
+      {data.pingData &&
+        <>
+          <Divider textAlign="left">
+              <Typography sx={{
+                color: 'text.secondary',
+                fontSize: '0.68rem'
+              }}>{`Players ${data.pingData?.players.online}/${data.pingData?.players.max}`}</Typography>
+          </Divider>
+          {data.pingData.players.sample?.map(player => {
+            return <Stack direction={'row'} spacing={1} key={player.id} sx={{alignItems: 'center'}}>
+              <MinecraftHead uuid={player.id}/>
+              <Typography>{player.name}</Typography>
+            </Stack>;
+          })}
+        </>
+      }
     </CardContent>
   </Card>
 }
